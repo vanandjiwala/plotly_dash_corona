940d734b7324af825761c4ff9126ca82b0cb74e3
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-03-25 00:24:21 +0530

    World chart implementation - Included latest dataset file

--- app.py
+++ app.py
@@ -16,6 +16,19 @@ df_timeseries = pd.read_csv('./Data/covid_19_india_1.csv')
 df_timeseries['Date']= pd.to_datetime(df_timeseries['Date'], format='%d/%m/%y')
 df_timeseries = df_timeseries.groupby(['Date']).sum().reset_index()
 
+#world data source
+
+df_world_input = pd.read_csv('./Data/time-series-19-covid-combined_csv.csv')
+df_world_summary = pd.DataFrame(columns=df_world_input.columns)
+country_group = df_world_input.groupby('Country/Region')
+for name, group in country_group:
+    df_world_summary = df_world_summary.append(group[group['Date'] == group['Date'].max()])
+df_world_summary.fillna(0, inplace=True)
+df_world_summary['Confirmed'] = df_world_summary['Confirmed'].astype(int)
+df_world_summary['Recovered'] = df_world_summary['Recovered'].astype(int)
+df_world_summary['Deaths'] = df_world_summary['Deaths'].astype(int)
+df_world_summary = df_world_summary.groupby('Country/Region').sum().reset_index()
+
 
 #Get rid of row containing total of columns
 df = df[df['Name of State / UT'] != 'Total number of confirmed cases in India']
@@ -30,16 +43,15 @@ for state in states:
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 app.title = 'My Title'
 
-#Setup static chart
+#Setup static india chart
 def setup_india_chart(dataframe):
-    df = dataframe
+
     data = []
     for cat in df.columns[3:5]:
-        print(cat)
         data.append(
             go.Bar(
-                x=df['Name of State / UT'],
-                y=df[cat],
+                x=dataframe['Name of State / UT'],
+                y=dataframe[cat],
                 name=cat,
                 textangle=45
             )
@@ -61,7 +73,7 @@ india_chart = setup_india_chart(df)
 
 #Setup timeseries chart
 def setup_india_timeseries_chart(dataframe):
-    #df_temp = dataframe
+
     data = [go.Scatter(
         x=dataframe['Date'],
         y=dataframe['ConfirmedIndianNational'],
@@ -93,6 +105,28 @@ def setup_india_timeseries_chart(dataframe):
 
 india_timeseries_progress = setup_india_timeseries_chart(df_timeseries)
 
+#Setup static india chart
+def setup_world_chart(dataframe):
+
+    data = []
+    data.append(
+        go.Bar(
+            x=dataframe['Country/Region'],
+            y=dataframe['Confirmed']
+        )
+    )
+
+    world_fig = {
+        'data': data,
+        'layout': go.Layout(
+            xaxis={'type': '-', 'title': 'Country'},
+            yaxis={'title': 'Case Count'}
+        )
+    }
+
+    return world_fig
+
+world_chart = setup_world_chart(df_world_summary)
 
 
 app.layout = html.Div(
@@ -182,18 +216,20 @@ app.layout = html.Div(
                 [dcc.Graph(id="india_trend_graph",figure=india_timeseries_progress)],
                 className="pretty_container five columns",
             )
-        ],className="row flex-display")
+        ],className="row flex-display"),
+        html.Div([
+            html.Div(
+                [dcc.Graph(id="world_graph", figure=world_chart)],
+                className="pretty_container twelve columns",
+            )
+        ], className="row flex-display")
     ],
         id="mainContainer",
         style={"display": "flex", "flex-direction": "column"}
 )
 
 
-
-
-
-
-
+#Callback function to update stat cards
 @app.callback([Output('totalLocalCasesText','children'),
                Output('totalForeignCasesText','children'),
                Output('totalCuredCasesText','children'),
@@ -206,10 +242,8 @@ def update_stats(state_name):
         result.append(df['Total Confirmed cases ( Foreign National )'].apply(pd.to_numeric).sum())
         result.append(df['Cured/Discharged/Migrated'].apply(pd.to_numeric).sum())
         result.append(df['Death'].apply(pd.to_numeric).sum())
-        print(result)
         return result
     else:
-        print("value selected {}".format(state_name))
         result.append(df[df['Name of State / UT'] == state_name]['Total Confirmed cases (Indian National)'].apply(pd.to_numeric))
         result.append(df[df['Name of State / UT'] == state_name]['Total Confirmed cases ( Foreign National )'].apply(pd.to_numeric))
         result.append(df[df['Name of State / UT'] == state_name]['Cured/Discharged/Migrated'].apply(pd.to_numeric))
@@ -221,11 +255,3 @@ def update_stats(state_name):
 
 if __name__ == '__main__':
     app.run_server()
-    # df = pd.read_csv('./Data/covid19_1.csv')
-    # print(df.columns)
-    # #getting rid of total row from dataframe
-    # df = df[df['Name of State / UT'] != 'Total number of confirmed cases in India']
-    # print(df[['Name of State / UT','Total Confirmed cases (Indian National)','Total Confirmed cases ( Foreign National )']])
-    #
-    #
-    # df = pd.read_csv
